6078c9f115dd930a45f16033001e17964c0f9e43
yashgarg30 <148379041+yashgarg30@users.noreply.github.com>
2025-10-28 09:51:38 +0530

    Update jwtMiddleware.js

---
+++ backend/src/utils/jwtMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access token required' });
+  }
+
+  jwt.verify(token, config.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Invalid or expired token' });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+function optionalAuth(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token) {
+    jwt.verify(token, config.JWT_SECRET, (err, user) => {
+      if (!err) {
+        req.user = user;
+      }
+    });
+  }
+  next();
+}
+
+module.exports = { authenticateToken, optionalAuth };
