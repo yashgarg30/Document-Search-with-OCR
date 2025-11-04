9829960d4aade3bad9bcd8a84fdd3b55f0323a5c
yashgarg30 <148379041+yashgarg30@users.noreply.github.com>
2025-11-04 10:42:02 +0530

    Create ProcessingStatus.jsx

---
+++ src/components/ProcessingStatus.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ProcessingStatus() {
+  return (
+    <div className="processing">
+      <div className="spinner"></div>
+      <p>Processing file, please wait...</p>
+    </div>
+  );
+}
